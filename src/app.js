dff5126bed1dedf5f626a6470c3f0bb651da0101
Venkatesh Mudam <mailtovenkateshpatel@gmail.com>
2025-05-30 01:57:54 -0500

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ app.use(express.json());
 app.use('/api', movieRoutes);
 
 const PORT = process.env.PORT || 3000;
+// Server connected to 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Movie API');
+  res.send('Welcome to the MOVIE API');
 });
